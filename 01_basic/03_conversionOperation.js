5d27c6151be7ae627b9746f8c336583c1332cb92
Haris Behzad <145331485+Harisbzd@users.noreply.github.com>
2024-09-08 11:56:53 +0000

    fourth

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -33,10 +33,40 @@ let isNotAvaliable = null;
 // boolean for null is always flase 
 
 
-let numberIsNotAvaliabel = Number(isNotAvaliable);
-console.log(numberIsNotAvaliabel);
+//let numberIsNotAvaliabel = Number(isNotAvaliable);
+//console.log(numberIsNotAvaliabel);
 
 // when  null is converted into interger it is zero 
 
 // when converting into string any thing can be converted into the string 
+
+
+// prefix and postfix //
+
+let x = 5;
+let y = ++x; // x is incremented to 6, then y is assigned the value 6
+// console.log(x); // Output: 6
+// console.log(y); // Output: 6
+
+
+let n = 5;
+let m = n++;
+
+// console.log(n);
+// console.log(m);
+
+// in prefix we can see that the value is first incrementes and the used 
+// but is post fix the valuse is first used and the incremented 
+
+
+console.log(1+2);
+console.log("1"+2);
+console.log("1"+"2");
+console.log(1+"2");
+console.log(1+2+"2");
+console.log("1"+2+2);
+
+
+
+
 
