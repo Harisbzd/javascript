76ca9243efb49a0ae9d9ed6bcd6be1f6d8ed8169
Haris Behzad <145331485+Harisbzd@users.noreply.github.com>
2024-09-08 10:58:44 +0000

    third

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,42 @@
+let score = "Haris"
+
+//console.log(typeof score)
+//console.log(typeof (score))
+
+let valueInNumber = Number(score);
+
+//console.log(valueInNumber);
+//console.log(typeof valueInNumber); // all though it says that is has been converted into number but when we check what is inside the memory location 
+
+// "33" when converted into integer => 33
+// "33abc" when connverted into integer give NaN which stands for not a number
+// boolean true when conveeted into intger is 1 
+// boolean false when converted itno intger is 0
+
+
+let isLoggedIn = "haris"
+let isNotLoggedIn = ""
+
+//let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+//let booleanIsNotLoggedIn = Boolean(isNotLoggedIn)
+//console.log( booleanIsNotLoggedIn)
+
+// when the string is empty it value is always false and when the strign is not empty the the value of the boolean is true 
+
+let isNotAvaliable = null;
+
+//let boolenIsNotAvaliable = Boolean(isNotAvaliable)
+//console.log(boolenIsNotAvaliable);
+
+// boolean for null is always flase 
+
+
+let numberIsNotAvaliabel = Number(isNotAvaliable);
+console.log(numberIsNotAvaliabel);
+
+// when  null is converted into interger it is zero 
+
+// when converting into string any thing can be converted into the string 
+
